b63300a4a56d8a4a349d5ad38700ade39954130b
Sruthi Gudiwada <gudiwadasruthi5@gmail.com>
2025-08-14 16:52:07 +0530

    .

--- backend/python.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "http://localhost:8000/analyze/"
-files = {'config': open('input/challenge1b_input.json', 'rb')}
-response = requests.post(url, files=files)
-print(response.json())
